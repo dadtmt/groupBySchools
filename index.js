a243572168c2bc752c8b1df5cab199dce02e9744
Thomas Culdaut <thomas@wildcodeschool.fr>
2019-05-23 10:19:46 +0200

    groupBySchools

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-const groupWildersBySchool = wilders => ({ wilders });
-const schoolsWithWilders = wilders => wilders;
+
+
+const groupWildersBySchool = wilders => wilders.reduce(
+  (acc, wilder) => (acc[wilder.schoolName]
+    ? {
+      ...acc,
+      [wilder.schoolName]: {
+        ...acc[wilder.schoolName],
+        wilders: [...acc[wilder.schoolName].wilders, wilder],
+      },
+    }
+    : {
+      ...acc,
+      [wilder.schoolName]: {
+        schoolName: wilder.schoolName,
+        wilders: [wilder],
+      },
+    }
+  ),
+  {},
+);
+const schoolsWithWilders = wilders => Object.values(
+  groupWildersBySchool(wilders),
+).sort((schoolA, schoolB) => (schoolA.schoolName < schoolB.schoolName ? -1 : 1));
 
 module.exports = { schoolsWithWilders, groupWildersBySchool };
